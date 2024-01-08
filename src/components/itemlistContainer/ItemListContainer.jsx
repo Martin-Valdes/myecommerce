5c153e9a47d8dbba5a00bc144fc82ca68014cb4b
fernando valdes <valdesmartin@gmail.com>
2024-01-08 08:28:05 -0300

    titulos agregados con condicionales en home

--- src/components/itemlistContainer/ItemListContainer.jsx
+++ src/components/itemlistContainer/ItemListContainer.jsx
@@ -10,15 +10,19 @@ const ItemListContainer = () =>{
 const [product, setProduct] = useState([]);
 const { categoria } = useParams();
 const [loading, setLoading] = useState(true);
+const [home, setHome] = useState(true);
+
 
 useEffect(()=>{
-
+    
     dataProduct
     .then((respuesta)=>{
         if(categoria){
             const filterProduct = respuesta.filter((producto)=> producto.categoria === categoria)
             setProduct(filterProduct)
+            setHome(false)
         }else{
+            setHome(true)
             setProduct(respuesta);
             
         }
@@ -40,11 +44,25 @@ useEffect(()=>{
                     <PacmanLoader color="rgba(54, 55, 214, 1)" size={40}/>
                 </div>
             ) : (   
-                <div className="divItem">
+                home ?(
+                    <div className="divItem">
                     <div className="itemListCont">
+                        <h3>Nuestros productos</h3>
                          <ItemList products = {product}/>
                      </div>
                 </div>
+                ):(
+                <div className="divItem">
+                    <div className="itemListCont">
+                        <h3>{categoria}</h3>
+                         <ItemList products = {product}/>
+                     </div>
+                </div>
+
+
+                )
+
+
             )}
         </>
     )
