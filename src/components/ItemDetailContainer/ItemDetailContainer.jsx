1ad0d4b74901143116ab3cdf0859ae274f810fad
Fernando Valdes <140764694+Martin-Valdes@users.noreply.github.com>
2024-03-22 07:07:37 -0300

    restore

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react"
-
 import { useParams } from "react-router-dom"
 import ItemDetail from "../ItemDetail/ItemDetail"
 import { doc, getDoc } from "firebase/firestore"
